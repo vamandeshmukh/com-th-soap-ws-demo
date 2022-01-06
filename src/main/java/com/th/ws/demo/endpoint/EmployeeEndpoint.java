6cbb01fcd3c5017b33c3092cc8559f1604176844
vamandeshmukh <vamandeshmukh@gmail.com>
2022-01-06 17:42:57 +0530

    C

--- src/main/java/com/th/ws/demo/endpoint/EmployeeEndpoint.java
+++ src/main/java/com/th/ws/demo/endpoint/EmployeeEndpoint.java
@@ -1,7 +1,6 @@
 package com.th.ws.demo.endpoint;
 
 import org.slf4j.Logger;
-
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
@@ -10,7 +9,7 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import com.th.ws.demo.model.Employee;
-import com.th.ws.demo.repository.EmployeeRepository;
+import com.th.ws.demo.service.EmployeeService;
 
 import https.www_torryharris_com.soap_ws_demo.EmployeeType;
 import https.www_torryharris_com.soap_ws_demo.GetEmployeeRequest;
@@ -23,12 +22,13 @@ public class EmployeeEndpoint {
 
 	private static final String NAMESPACE_URI = "https://www.torryharris.com/soap-ws-demo";
 
-	private EmployeeRepository employeeRepository;
+	@Autowired
+	EmployeeService employeeService;
 
 	@Autowired
-	public EmployeeEndpoint(EmployeeRepository employeeRepository) {
+	public EmployeeEndpoint(EmployeeService employeeService) {
 		LOG.info("2 EmployeeEndpoint");
-		this.employeeRepository = employeeRepository;
+		this.employeeService = employeeService;
 	}
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getEmployeeRequest")
@@ -36,12 +36,14 @@ public class EmployeeEndpoint {
 	public GetEmployeeResponse getEmployee(@RequestPayload GetEmployeeRequest request) {
 		LOG.info("...5 getEmployee " + request.getEmployeeId());
 		GetEmployeeResponse response = new GetEmployeeResponse();
-		Employee emp = employeeRepository.findById(request.getEmployeeId()).get();
-//		EmployeeType empt = (EmployeeType) employeeRepository.findById(request.getEmployeeId()).get();
+		Employee emp = employeeService.getEmployeeById(request.getEmployeeId());
+		LOG.info(emp.toString());
+//		EmployeeType empt = emp;
 		EmployeeType empt = new EmployeeType();
-		empt.setEmployeeId(101);
+		empt.setEmployeeId(11);
 		empt.setFirstName("Sonu");
 		empt.setSalary(50000);
+		LOG.info(empt.toString());
 		response.setEmployeeType(empt);
 		return response;
 	}
--- src/main/java/com/th/ws/demo/repository/EmployeeRepository.java
+++ src/main/java/com/th/ws/demo/repository/EmployeeRepository.java
@@ -1,51 +1,10 @@
 package com.th.ws.demo.repository;
 
-//import java.util.HashMap;
-//import java.util.Map;
-//import javax.annotation.PostConstruct;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.stereotype.Component;
-//import org.springframework.util.Assert;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.th.ws.demo.model.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-//	Logger LOG = LoggerFactory.getLogger(this.getClass());
-//
-//	private static final Map<String, Employee> employees = new HashMap<>();
-//
-//	@PostConstruct
-//	public void initData() {
-//		LOG.info("1 initData");
-//		Employee e1 = new Employee();
-//		e1.setEmployeeId(101);
-//		e1.setFirstName("Sonu");
-//		e1.setSalary(55000);
-//		employees.put(e1.getFirstName(), e1);
-//
-//		Employee e2 = new Employee();
-//		e2.setEmployeeId(102);
-//		e2.setFirstName("Monu");
-//		e2.setSalary(50000);
-//		employees.put(e2.getFirstName(), e2);
-//
-//		Employee e3 = new Employee();
-//		e3.setEmployeeId(103);
-//		e3.setFirstName("Tonu");
-//		e3.setSalary(60000);
-//		employees.put(e3.getFirstName(), e3);
-//	}
-//
-//	public Employee findEmployee(String firstName) {
-//		LOG.info("...6 findEmployee");
-//		Assert.notNull(firstName, "The employee's name must not be null.");
-//		return employees.get(firstName);
-//	}
 
 }
